bf24b3a32561c15d6c6d6461a2542558fb220e8d
mihadadene <mi.ihadadene@gmail.com>
2020-04-01 23:33:47 -0400

    first commit

---
+++ TestCnxBd/Produit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCnxBd
+{
+    public class Produit
+    {
+
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+
+        public int Qty { get; set; }
+
+        public double Prix { get; set; }
+
+        public Produit()
+        {
+
+        }
+    }
+}
---
+++ TestCnxBd/Program.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+
+using System.Collections.Generic;
+using System.Data;
+
+namespace TestCnxBd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetListProduit();
+        }
+        public static List<Produit> GetListProduit()
+        {
+            List<Produit> result = new List<Produit>();
+            Produit produit; 
+            MySqlConnection cnx = new MySqlConnection();
+            cnx.ConnectionString = "Server = 127.0.0.1; Uid = root; password = root; Database = mysql; ";
+            cnx.Open();
+
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.CommandText = "Produits";
+            cmd.CommandType = CommandType.TableDirect;
+            cmd.Connection = cnx;
+
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                produit = new Produit();
+                produit.Id = int.Parse(dr["IdProd"].ToString());
+                produit.Nom = dr["NomProd"].ToString();
+                produit.Qty = int.Parse(dr["QtyProd"].ToString());
+                produit.Prix = double.Parse(dr["PrixProd"].ToString());
+
+                result.Add(produit);
+            }
+
+            cnx.Close();
+            //Console.WriteLine(result);
+            return result;
+        }
+    }
+
+}   
+
